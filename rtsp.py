4b307632c384af5106f5b44134c07e1b9096e65b
Colditz Colligula <79027579+ColditzColligula@users.noreply.github.com>
2022-04-05 16:32:18 +0200

    Add files via upload

--- rtsp.py
+++ rtsp.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
+from tarfile import NUL
 import PySimpleGUI as sg
 import cv2
 import webbrowser
 import pyperclip
+import os 
+import requests
 
 # Simple example of TabGroup element and the options available to it
 
 sg.theme('Dark Amber')     # Please always add color to your window
 
-#try:
-#    splashscreen = ("splashscreen.png")
-#    DISPLAY_TIME_MILLISECONDS = 1000
-#
-#    sg.Window('Window Title', [[sg.Image(splashscreen)]], transparent_color=sg.theme_background_color(), no_titlebar=True, keep_on_top=True).read(timeout=DISPLAY_TIME_MILLISECONDS, close=True)
-#except:
-#    pass
+
+try:
+    splashscreen = "splashscreen.png"
+    isExist = os.path.exists(splashscreen)
+    if isExist == True:
+        DISPLAY_TIME_MILLISECONDS = 600
+        sg.Window('Window Title', [[sg.Image(splashscreen)]], transparent_color=sg.theme_background_color(), no_titlebar=True, keep_on_top=True).read(timeout=DISPLAY_TIME_MILLISECONDS, close=True)
+    elif isExist == False:
+        pass
+except:
+    pass
 
 def opencamerastream():
     global ADDRESS, USERNAME, PASSWORD, CHANNELSELECT
@@ -37,7 +44,13 @@ def opencamerastream():
 
 # The tab 1, 2, 3 layouts - what goes inside the tab
 
-
+tab0_layout = [[sg.Text('Camera Maintenance')],      
+        [sg.Text('IP Address'), sg.Input(key='-ADDRESSMAINT-')],
+        [sg.Text('Username'), sg.Input(key='-USERNAMEMAINT-')],
+        [sg.Text('Password'), sg.Input(password_char = "•", key='-PASSWORDMAINT-')],
+        [sg.Button('Serial No.')]]
+       
+        #[sg.Button('Open'), sg.Button('Copy RTSP Link'), sg.Button('Web Interface'), sg.Button('Exit', key='-EXIT-'), sg.Button('Help')]]
 
 tab1_layout = [[sg.Text('RTSP Stream')],      
         [sg.Text('IP Address & Port'), sg.Input(key='-ADDRESS-')],
@@ -54,15 +67,18 @@ tab1_layout = [[sg.Text('RTSP Stream')],
                 #sg.Tab('Record Time', tab2_recordtime, key='-RECORDTIME-'),
                 #sg.Tab('Disk Array', tab2_diskarray, key='-DISKARRAY-')]
 tab2_layout = [[sg.Text('Capacity Calculation')],
-                [sg.Text('Bandwith Calculation - WORK IN PROGRESS')],
+                [sg.Text('Bandwidth Calculation - (High Quality / 25 fps)')],
                 [sg.Text('Resolution'), sg.Text('# of Cameras'), sg.Text('Codec')],
-                [sg.Text('1 Megapixel'), sg.Input('', key='-#1MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#1MPh265'), sg.Radio('H.264', 'CODECSEL', key='#1MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#1MPMJPEG')],
-                [sg.Text('2 Megapixel'), sg.Input('', key='-#2MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#2MPh265'), sg.Radio('H.264', 'CODECSEL', key='#2MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#2MPMJPEG')],
-                [sg.Text('4 Megapixel'), sg.Input('', key='-#4MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#4MPh265'), sg.Radio('H.264', 'CODECSEL', key='#4MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#4MPMJPEG')],
-                [sg.Text('5 Megapixel'), sg.Input('', key='-#5MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#5MPh265'), sg.Radio('H.264', 'CODECSEL', key='#5MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#5MPMJPEG')],
-                [sg.Text('6 Megapixel'), sg.Input('', key='-#6MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#6MPh265'), sg.Radio('H.264', 'CODECSEL', key='#6MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#6MPMJPEG')],
-                [sg.Text('8 Megapixel'), sg.Input('', key='-#8MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#8MPh265'), sg.Radio('H.264', 'CODECSEL', key='#8MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#8MPMJPEG')],
-                [sg.Text('12 Megapixel'), sg.Input('', key='-#12MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL', default=True, key='#12MPh265'), sg.Radio('H.264', 'CODECSEL', key='#12MPh264'), sg.Radio('MJPEG', 'CODECSEL', key='#12MPMJPEG')]]
+                [sg.Text('1 Megapixel'), sg.Input('', key='-#1MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL1', default=True, key='#1MPh265', metadata="1024"), sg.Radio('H.264', 'CODECSEL1', key='#1MPh264'), sg.Radio('MJPEG', 'CODECSEL1', key='#1MPMJPEG')],
+                [sg.Text('2 Megapixel'), sg.Input('', key='-#2MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL2', default=True, key='#2MPh265'), sg.Radio('H.264', 'CODECSEL2', key='#2MPh264'), sg.Radio('MJPEG', 'CODECSEL2', key='#2MPMJPEG')],
+                [sg.Text('4 Megapixel'), sg.Input('', key='-#4MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL4', default=True, key='#3MPh265'), sg.Radio('H.264', 'CODECSEL4', key='#4MPh264'), sg.Radio('MJPEG', 'CODECSEL4', key='#4MPMJPEG')],
+                [sg.Text('5 Megapixel'), sg.Input('', key='-#5MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL5', default=True, key='#5MPh265'), sg.Radio('H.264', 'CODECSEL5', key='#5MPh264'), sg.Radio('MJPEG', 'CODECSEL5', key='#5MPMJPEG')],
+                [sg.Text('6 Megapixel'), sg.Input('', key='-#6MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL6', default=True, key='#6MPh265'), sg.Radio('H.264', 'CODECSEL6', key='#6MPh264'), sg.Radio('MJPEG', 'CODECSEL6', key='#6MPMJPEG')],
+                [sg.Text('8 Megapixel'), sg.Input('', key='-#8MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL8', default=True, key='#8MPh265'), sg.Radio('H.264', 'CODECSEL8', key='#8MPh264'), sg.Radio('MJPEG', 'CODECSEL8', key='#8MPMJPEG')],
+                [sg.Text('12 Megapixel'), sg.Input('', key='-#12MP-', size=(4, 1)), sg.Radio('H.265', 'CODECSEL12', default=True, key='#12MPh265'), sg.Radio('H.264', 'CODECSEL12', key='#12MPh264'), sg.Radio('MJPEG', 'CODECSEL12', key='#12MPMJPEG')],
+                [sg.Button('Calculate', key='-BANDWIDTHCALCULATE-'), sg.Button('Exit', key='-EXIT2-')]]
+
+
 
 
 
@@ -77,8 +93,14 @@ tab6_layout = [[sg.Text('Controlex CCTV Companion\n\n'
                         'Version 0.1\n')]]
 
 
+#   Bandwidth Calculation
+oneMPh265 = 1024
+
+
+
 # The TabgGroup layout - it must contain only Tabs
-tab_group_layout = [[sg.Tab('RTSP Stream', tab1_layout, key='-TAB1-'),
+tab_group_layout = [[sg.Tab('Camera Maintenance', tab0_layout, key='-TAB0-'),
+                     sg.Tab('RTSP Stream', tab1_layout, key='-TAB1-'),
                      sg.Tab('Capacity Calculation', tab2_layout, key='-TAB2-'),
                      sg.Tab('IP Calculation', tab3_layout, key='-TAB3-'),
                      sg.Tab('Lens Calculation', tab4_layout, key='-TAB4-'),
@@ -94,11 +116,21 @@ layout = [[sg.TabGroup(tab_group_layout,
 
 window = sg.Window('CCTV Companion', layout, no_titlebar=False)
 
-tab_keys = ('-TAB1-','-TAB2-','-TAB3-', '-TAB4-','-TAB5-','-TAB6-',)         # map from an input value to a key
+tab_keys = ('-TAB0-','-TAB1-','-TAB2-','-TAB3-', '-TAB4-','-TAB5-','-TAB6-',)         # map from an input value to a key
 
 while True:
     event, values = window.read()
     #print(event, values)
+
+    #   Camera Maintenance
+    if event == 'Serial No.':
+        ADDRESS = values['-ADDRESSMAINT-']
+        USERNAME = values['-USERNAMEMAINT-']
+        PASSWORD = values['-PASSWORDMAINT-']
+        serialno = requests.get('http://admin:123456789a@192.168.178.222/cgi-bin/magicBox.cgi?action=getSerialNo')
+        #serialno = requests.get("http://"+USERNAME+":"+PASSWORD+"@"+ADDRESS+"/cgi-bin/magicBox.cgi?action=getSerialNo")
+        sg.PopupOK(serialno)
+
     if event == 'Open':
         ADDRESS = values['-ADDRESS-']
         USERNAME = values['-USERNAME-']
@@ -131,6 +163,24 @@ while True:
             "If you click on the Button 'Web Interface' it'll open your default Webbrowser and navigate you to the entered IP-Address\n\n"
             "You can select which Stream you want to see by clicking either 'Main Stream' or 'Sub Stream'\n")
 
+    if event == '-BANDWIDTHCALCULATE-':
+        #BandwidthCalculationResult = 
+                try:
+                    if int(values['-#1MP-']) > 0:
+                        result = int(values['-#1MP-']) * 1024
+                        [sg.PopupOK(result)]
+                except:
+                    if len(values['-#1MP-']) == 0:
+                        [sg.PopupError('You must input a number of cameras')]
+        
+
+
+
+
+        # 
+        # int(values['-#1MP-']) * int(metadata['#1MPh265'])
+                
+
     if event == '-CONTROLEXWEBSITE-':
         webbrowser.open('https://controlex.eu')
 
@@ -144,7 +194,7 @@ while True:
         [sg.Popup('Controlex\n\n'
                     'Use the links on this Site to quickly navigate to our Website, Online-Shop or Helpdesk\n')]
 
-    if event == sg.WIN_CLOSED or event == '-EXIT-' or event == '-EXIT5-':
+    if event == sg.WIN_CLOSED or event == '-EXIT-' or event == '-EXIT2-' or event == '-EXIT3-' or event == '-EXIT4-' or event == '-EXIT5-' or event == '-EXIT6-':
         break
     # handle button clicks
     #if event == 'Invisible':
